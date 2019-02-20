f56d33f36f4434e6ea8f88e69b57cd73bc9ea8bd
Adam Sheagren <adamms0521@gmail.com>
2019-02-20 16:17:29 -0600

    created project

---
+++ src/main/java/edu/uiowa/projectteam10/Team10Project/Team10ProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.uiowa.projectteam10.Team10Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team10ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team10ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uiowa/projectteam10/Team10Project/Team10ProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.uiowa.projectteam10.Team10Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Team10ProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
